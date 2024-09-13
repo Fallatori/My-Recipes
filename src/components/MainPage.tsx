a4a952fb8939c5882f5e51625eb43621ada3c4b1
Falla <farninn@gmail.com>
2024-09-13 10:31:18 +0200

    Fix crash on searchFilter

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -6,38 +6,38 @@ import RecipeList from "./RecipeList";
 import RecipeFilterBar from "./global/RecipeFilterbar";
 
 interface Props {
-  recipes: RecipeType[];
+	recipes: RecipeType[];
 }
 
 export const MainPage = ({ recipes }: Props) => {
-  const [query, setQuery] = useState("");
+	const [query, setQuery] = useState("");
 
-  const searchFilter = (recipes: RecipeType[]) => {
-    return recipes.filter(
-      (recipe) =>
-        recipe.headline.toLowerCase().includes(query) ||
-        recipe.tags.find((tag) => tag.value === query)
-    );
-  };
+	const searchFilter = (recipes: RecipeType[]) => {
+		return recipes.filter(
+			(recipe) =>
+				recipe.headline.toLowerCase().includes(query) ||
+				recipe?.tags?.find((tag) => tag.value === query)
+		);
+	};
 
-  const filtered = searchFilter(recipes);
+	const filtered = searchFilter(recipes);
 
-  // send this to SearchInput to change the query
-  const handleChange = (e: any) => {
-    setQuery(e.target.value.toLowerCase());
-  };
+	// send this to SearchInput to change the query
+	const handleChange = (e: any) => {
+		setQuery(e.target.value.toLowerCase());
+	};
 
-  return (
-    <>
-      <div className="flex justify-center my-4 text-3xl text-accent-1 md:hidden">
-        <h1>My Recipes</h1>
-      </div>
-      <RecipeFilterBar
-        searchQuery={handleChange}
-        setQuery={setQuery}
-        query={query}
-      />
-      <RecipeList recipes={filtered} />
-    </>
-  );
+	return (
+		<>
+			<div className="flex justify-center my-4 text-3xl text-accent-1 md:hidden">
+				<h1>My Recipes</h1>
+			</div>
+			<RecipeFilterBar
+				searchQuery={handleChange}
+				setQuery={setQuery}
+				query={query}
+			/>
+			<RecipeList recipes={filtered} />
+		</>
+	);
 };
